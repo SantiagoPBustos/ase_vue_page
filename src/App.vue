5154c6a4e2eec64b22f239f52b6e9cc3b93a92e9
Santiago <david.pina@uptc.edu.co>
2024-06-11 00:31:15 -0500

    load project to github

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav :class="menuClass" ref="menu">
+      <button class="nav-toggle">
+        <img class="img-menu-button" src="@/assets/imgs/menu.png" width="30px" />
+      </button>
+      <ul class="nav-menu">
+        <li>
+          <RouterLink class="selected-item" to="/">Principal</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/">¿Quienes Somos?</RouterLink>
+        </li>
+        <li><RouterLink to="/projects">Proyectos</RouterLink></li>
+        <li><RouterLink to="/locateUS">Ubiquenos</RouterLink></li>
+        <li><RouterLink to="/locateUS">Contactenos</RouterLink></li>
+        <RouterLink to="/" class="img-logo-navbar"
+          ><img src="@/assets/imgs/LogoASE.png"
+        /></RouterLink>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="whatsapp-btn">
+    <a href="https://wa.me/+573102467622" target="_blank">
+      <img src="@/assets/bg_wp.png" alt="WhatsApp Icon" />
+    </a>
+  </div>
+
+  <div id="miPopup" class="popup">
+    <p>Contactanos!</p>
+  </div>
+
+  <RouterView />
+  <article ref="ContactUSId" id="ContactUSId" class="shadow principal-banner">
+    <div class="background">
+      <h1>Contactenos</h1>
+    </div>
+  </article>
+
+  <article class="form-contact-seccion">
+    <form>
+      <input type="text" placeholder="Nombre Completo" />
+      <input type="text" placeholder="Telefono" />
+      <input type="email" placeholder="Correo Electronico" />
+      <textarea placeholder="Descripción" rows="4" cols="50"></textarea>
+      <input value="Solicitar Cotización" class="button-submit" type="submit" />
+    </form>
+  </article>
+  <footer class="footer">
+    <div><img class="cont-logo-footer" src="@/assets/imgs/LogoASE.png" /></div>
+
+    <div class="cont-contact">
+      <div class="contact-seccion contact-first-seccion">
+        <p>
+          <span class="text-gold">Contactanos</span><br />
+          <a href="tel:(+57)3204234160">(+57) 320 423 4160</a><br />
+          <a href="tel:(+57)3102467622">(+57) 310 246 7622</a><br />
+          <a href="mailto:acabadosysolucionesexitoase@gmail.com">
+            acabadosysolucionesexitoase@gmail.com </a
+          ><br />
+          <a href="https://www.facebook.com/profile.php?id=100084519013222" target="_blank">
+            <img src="@/assets/imgs/Icon-Facebook.png" alt="linkFacebbok" class="size-icon" />
+            <span class="text-icon">Facebook</span>
+          </a>
+          <a href="https://www.instagram.com/acabados_ysolucionesexito/" target="_blank">
+            <img src="@/assets/imgs/Icon-Instagram.png" alt="linkInstagram" class="size-icon" />
+            <span>Instagram</span>
+          </a>
+        </p>
+      </div>
+
+      <div class="contact-seccion contact-second-seccion">
+        <p>
+          <span class="text-gold">Tunja - Boyacá</span><br /><br />
+          <b
+            >Avenida Carrera 6 N° 59-47 <br />
+            Barrio Villa Luz</b
+          >
+        </p>
+      </div>
+    </div>
+
+    <div class="cont-copyrigth">
+      <p>
+        El uso de este sitio web implica la aceptación de los
+        <a href="files/Terminos&Condiciones.pdf">Términos y Condiciones</a>
+        y Políticas de privacidad de ACABADOS Y SOLUCIONES EXITO.
+        <b>© 2022 Todos los derechos reservados.</b> Desarollado por
+        <a href="mailto: dasapibu@gmail.com">David Santiago Piña Bustos</a>
+        .Prohibida su reproducción total o parcial, así como su traducción a cualquier idioma sin la
+        autorización escrita de su titular.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuClass: ''
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initMenu()
+    })
+  },
+  methods: {
+    initMenu() {
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    handleScroll() {
+      const menuElement = this.$refs.menu
+      const height = menuElement.offsetTop
+
+      if (window.pageYOffset > height) {
+        this.menuClass = 'menu-fixed'
+      } else {
+        this.menuClass = ''
+      }
+    }
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,202 @@
+<template>
+  <article>
+    <section class="curved">
+      <div><img class="logo" src="@/assets/imgs/LogoASE.png" /></div>
+      <div class="text">
+        <p>Elaboramos tus proyectos en tiempo requerido y con altos estándares de calidad.</p>
+      </div>
+      <div class="buttons">
+        <RouterLink to="/projects">Nuestros Trabajos</RouterLink>
+        <a @click="scrollToTarget('ContactUSId')">Contactanos</a>
+      </div>
+    </section>
+
+    <section id="jobs" class="cont-swiper jobs">
+      <div class="title-seccion">
+        <h1>Portafolio de Servicios y Proyectos</h1>
+      </div>
+      <div class="line-black"></div>
+
+      <div class="container-slide-services">
+        <swiper
+          :effect="'coverflow'"
+          :grabCursor="true"
+          :centeredSlides="true"
+          :slidesPerView="'auto'"
+          :coverflowEffect="{
+            rotate: 25,
+            stretch: -50,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false
+          }"
+          :pagination="true"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Barandas_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Cubiertas_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Divisiones_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Persianas_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Pisos_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/PortonesElectricos_Icon.png"
+          /></swiper-slide>
+          <swiper-slide class="swiper-slide-services"
+            ><img src="@/assets/imgs/Techos_Icon.png"
+          /></swiper-slide>
+        </swiper>
+      </div>
+    </section>
+
+    <section id="AboutUS" class="cont-aboutUS">
+      <div>
+        <h1>¿Quienes Somos?</h1>
+      </div>
+      <div class="container-aboutUS">
+        <div class="cont-first-AboutUS">
+          <h3 class="title-AboutUS">Nosotros</h3>
+          <p>
+            Somos una empresa con más de 10 años en el mercado dedicada a la manufactura e
+            instalación de todo tipo de acabados para construcciones, contamos con personal
+            capacitado para la producción e instalación optima a nivel nacional.
+          </p>
+        </div>
+        <div class="cont-second-AboutUS">
+          <h3 class="title-AboutUS">Misión</h3>
+          <p>
+            Nuestro principal enfoque es satisfacer la necesidad de nuestros clientes mediante
+            estándares de calidad y puntualidad en los proyectos y obras relacionadas con los
+            acabados para la construcción.
+          </p>
+        </div>
+        <div class="cont-third-AboutUS">
+          <h3 class="title-AboutUS">Visión</h3>
+          <p>
+            Para el 2027 ser una empresa líder en la región, en cuanto a importación, distribución y
+            capacitación de sistemas de automatización de acceso vehicular y peatonal a nivel
+            residencial, comercial e industrial.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="cont-jobs bg-gray">
+      <div class="title-seccion">
+        <h1>Proyectos</h1>
+      </div>
+      <div class="line-black"></div>
+      <div class="container-jobs-slide">
+        <div class="cont-slipe">
+          <swiper
+            :spaceBetween="20"
+            :centeredSlides="true"
+            :autoplay="{
+              delay: 5000,
+              disableOnInteraction: false
+            }"
+            :pagination="{
+              clickable: true
+            }"
+            :navigation="true"
+            :modules="modules"
+            class="mySwiper"
+          >
+            <swiper-slide>
+              <div class="swiper-slide swiper-slide-img mwh">
+                <img src="@/assets/imgs/jobs/EspejoMadera.jpg" />
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+              <div class="swiper-slide swiper-slide-img mwh">
+                <img src="@/assets/imgs/jobs/Espejo.jpg" /></div
+            ></swiper-slide>
+            <swiper-slide>
+              <div class="swiper-slide swiper-slide-img mwh">
+                <img src="@/assets/imgs/jobs/Glamping.jpg" /></div
+            ></swiper-slide>
+            <swiper-slide>
+              <div class="swiper-slide swiper-slide-img mwh">
+                <img src="@/assets/imgs/jobs/Ventana_1.jpg" /></div
+            ></swiper-slide>
+            <swiper-slide>
+              <div class="swiper-slide swiper-slide-img mwh">
+                <img src="@/assets/imgs/jobs/Muebles.jpg" /></div
+            ></swiper-slide>
+          </swiper>
+        </div>
+        <div class="descripcion-jobs">
+          <p>
+            Somos <b>Acabados y Soluciones Exito</b>, brindamos asesoramiento en diseño y solucion a
+            tus espacios. Elaboramos tus proyectos en <b>tiempo</b> requerido y con altos estándares
+            de <b>calidad</b>.
+          </p>
+        </div>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-coverflow'
+import '@/assets/main.css'
+import { EffectCoverflow, Autoplay, Pagination, Navigation } from 'swiper/modules'
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  setup() {
+    return {
+      modules: [EffectCoverflow, Autoplay, Pagination, Navigation]
+    }
+  },
+  methods: {
+    scrollToTarget(id) {
+      const targetElement = this.$refs.targetElement
+      const targetElementId = document.getElementById(id)
+      window.scrollTo({
+        top: targetElementId.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 200px;
+}
+
+.container-slide-services {
+  width: 100%;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+}
+
+.mwh {
+  width: 100%;
+  height: 100%;
+}
+</style>
